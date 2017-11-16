6da0a4e9dc748149a3edf8b475f8aa80b4508ebe
Dennis Saenger <github.com@mail.ds82.de>
2017-11-16 13:01:25 +0100

    feat: add datetime helper functions

---
+++ jslib/datetime.js
@@ -0,0 +1,68 @@
+load('./../conf/automation/jsr223/jslib/moment.js');
+
+moment.locale('de');
+
+function momentFromItem(item) {
+  return moment(ir.getItem(item).state, moment.ISO_8601);
+}
+
+function isSameDay(day1, day2) {
+  day2 = day2 || moment();
+  return moment(day1).isSame(day2, 'day');
+}
+
+function isWorkDay(day) {
+  day = day || DateTime.now();
+  var weekday = moment(day).weekday();
+  return weekday > 0 && weekday <= 5;
+}
+
+function isBeforeSunrise(when) {
+  when = !!when ? moment(when) : moment();
+  return when.isBefore(momentFromItem('Astro_SunriseTime'));
+}
+
+function isBeforeSunset(when) {
+  when = !!when ? moment(when) : moment();
+  return when.isBefore(momentFromItem('Astro_SunsetTime'));
+}
+
+function isAfterSunrise(when) {
+  when = !!when ? moment(when) : moment();
+  return when.isAfter(momentFromItem('Astro_SunriseTime'));
+}
+
+function isAfterSunset(when) {
+  when = !!when ? moment(when) : moment();
+  return when.isAfter(momentFromItem('Astro_SunsetTime'));
+}
+
+function isBefore(pointInTime, anytimeBefore) {
+  pointInTime = moment.isMoment(pointInTime)
+    ? pointInTime
+    : moment(pointInTime);
+  anytimeBefore = !!anytimeBefore ? moment(anytimeBefore) : moment();
+  return anytimeBefore.isBefore(pointInTime);
+}
+
+function isOlderThanSeconds(d1, d2, secs) {
+  var m1 = moment(d1);
+  var m2 = moment(d2);
+  return m1.diff(m2, 'seconds') > secs;
+}
+
+function isBetweenHourOfDay(hour1, hour2) {
+  var hourNow = moment().get('hour');
+  return hour1 <= hourNow && hourNow <= hour2;
+}
+
+function isTomorrow(dateTimeItem) {
+  var d = momentFromItem(dateTimeItem);
+  var tomorrow = moment(DateTime.now()).add(1, 'days');
+
+  return d.isSame(tomorrow, 'days');
+}
+
+function diffSeconds(d1, d2) {
+  return Math.abs(moment(d1).diff(d2, 'seconds'));
+}
